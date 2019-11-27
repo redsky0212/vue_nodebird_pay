99bf03c8821e467aa910988a427e88247d794b22
redsky <hyun0238@gmail.com>
2019-11-27 17:58:42 +0900

    eslint관련설청추가

---
+++ ch1/front/components/FollowList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list>
+    <ul>
+      <li>
+        <span>제로초</span>
+        <v-icon>mdi-minus-circle-outline</v-icon>
+      </li>
+      <li>
+        <span>네로</span>
+        <v-icon>mdi-minus-circle-outline</v-icon>
+      </li>
+      <li>
+        <span>히어로</span>
+        <v-icon>mdi-minus-circle-outline</v-icon>
+      </li>
+    </ul>
+  </v-list>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
